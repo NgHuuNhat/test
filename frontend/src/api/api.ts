dce4e8b9a0f8351a373965262c2e6e710fb7e3cb
Ng Huu Nhat <nhat200901@gmail.com>
2025-03-31 11:38:45 +0700

    add

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:1337/api";
+
+export const getPosts = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/products`);
+        console.log("Response từ API:", response.data.data);
+        return response.data.data;
+    } catch (error) {
+        console.error("Lỗi khi lấy dữ liệu:", error);
+        return [];
+    }
+};
---
+++ frontend/src/components/index.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { getPosts } from '../api/api';
+import { Product } from '../types/type';
+
+export default function Test() {
+    const [products, setPosts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await getPosts();
+            setPosts(data);
+            console.log(data)
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Danh sách sản phẩm</h1>
+            <ul>
+                {products.map((product) => (
+                    <li key={product.id}>
+                        <h2>{product.name}</h2>
+                        <p>Giá: {product.price.toLocaleString()} VND</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
