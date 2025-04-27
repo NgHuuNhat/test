67d1370145468d861ab9c70a5f2823c1044be123
Ng Huu Nhat <nhat200901@gmail.com>
2025-04-28 04:43:10 +0700

    update

--- frontend/src/home/components/ProductCard.tsx
+++ frontend/src/home/components/ProductCard.tsx
@@ -34,18 +34,24 @@ export default function ProductCard({
         className="w-full h-full object-cover cursor-pointer transition-transform duration-300 group-hover:scale-105"
       />
 
-      <div className="absolute bottom-0 left-0 right-0 px-2 py-2 bg-gradient-to-t from-black/70 to-transparent text-white text-sm flex justify-between items-center">
-        <span className="font-semibold text-l">${product.price}</span>
-        <span className="font-semibold text-l ms-10">{product.name}</span>
+      <div className='absolute top-0 right-0 px-2 py-2  text-white text-sm flex justify-between items-center'>
+        <button onClick={handleAddToWish} className="p-2 text-white/100 rounded-full hover:text-white/50 cursor-pointer transition-transform hover:scale-105">
+          <Heart size={26} />
+        </button>
+      </div>
 
-        <div className="flex gap-1">
-          <button onClick={handleAddToWish} className="p-2 text-white/100 rounded-full hover:text-white/50 cursor-pointer transition-transform hover:scale-105">
-            <Heart size={26} />
-          </button>
-          <button onClick={handleAddToCart} className="p-2 text-white/100 rounded-full hover:text-white/50 cursor-pointer transition-transform hover:scale-105">
-            <ShoppingCart size={26} />
+      <div className="absolute bottom-0 left-0 right-0 px-2 py-2 bg-gradient-to-t from-black/70 to-transparent text-white text-sm items-center">
+        <div className='flex justify-between'>
+          <span className="font-semibold text-l">{product.name}</span>
+          <span className="font-semibold text-l">${product.price}</span>
+        </div>
+
+        <div>
+          <button onClick={handleAddToCart} className="bg-white/10 w-full flex justify-center items-center p-2 text-white/100 rounded-full hover:text-white/50 cursor-pointer transition-transform hover:scale-105">
+            <ShoppingCart className='text-center' size={26} />
           </button>
         </div>
+
       </div>
     </div>
   );
