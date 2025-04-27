2c98f2496855dd26092d7abc5f22be6acc71ff0e
Ng Huu Nhat <nhat200901@gmail.com>
2025-04-28 05:32:02 +0700

    update

--- frontend/src/home/components/ProductCard.tsx
+++ frontend/src/home/components/ProductCard.tsx
@@ -34,22 +34,19 @@ export default function ProductCard({
         className="w-full h-full object-cover cursor-pointer transition-transform duration-300 group-hover:scale-105"
       />
 
-      <div className='absolute top-0 right-0 px-2 py-2  text-white text-sm flex justify-between items-center'>
-        <button onClick={handleAddToWish} className="p-2 text-white/100 rounded-full hover:text-white/50 cursor-pointer transition-transform hover:scale-105">
+      <div className='absolute top-0 right-0 text-white text-sm flex justify-between items-center'>
+        <button onClick={handleAddToWish} className="p-4 text-white/100 rounded-full hover:text-white/50 cursor-pointer transition-transform hover:scale-105">
           <Heart size={26} />
         </button>
       </div>
 
-      <div className="absolute bottom-0 left-0 right-0 px-2 py-2 bg-gradient-to-t from-black/70 to-transparent text-white text-sm items-center">
-        <div className='flex justify-between'>
+      <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/70 to-transparent text-white text-sm items-center">
+        <div className="w-full flex justify-between items-center pb-4 px-4">
           <span className="font-semibold text-l">{product.name}</span>
-          <span className="font-semibold text-l">${product.price}</span>
-        </div>
-
-        <div>
-          <button onClick={handleAddToCart} className="bg-white/10 w-full flex justify-center items-center p-2 text-white/100 rounded-full hover:text-white/50 cursor-pointer transition-transform hover:scale-105">
-            <ShoppingCart className='text-center' size={26} />
+          <button onClick={handleAddToCart} className="w-full max-w-[40%] flex justify-center items-center bg-white/10 py-2 rounded-full text-white transition-transform duration-200 hover:scale-105 hover:text-white/50 cursor-pointer">
+            <ShoppingCart className='w-full' size={26} />
           </button>
+          <span className="font-semibold text-l">${product.price}</span>
         </div>
 
       </div>
